c919cbedab15660ba79464a248bb5594757b4f46
dondasasmita <dondasasmita@gmail.com>
2020-06-29 11:20:35 +0800

    demo of webpack and vue cli

---
+++ 12/src/Amount.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <div class="columns">
+      <div class="column is-6">
+        <input v-model="amount" class="input is-primary" type="number" placeholder="enter the amount" @keyup="calculateTotal"></input>
+      </div>
+      <div class="column is-6">
+        <div class="tags has-addons">
+          <span class="tag">Powered by</span>
+          <span class="tag is-primary">Bulma</span>
+        </div>
+      </div>
+    </div>
+    <div class="columns" v-show="this.gst > 0">
+      <div class="column is-centered">
+        <h5>Total GST is <span class="tag is-success">{{this.gst}}</span></h5>
+        <h5>Total Bill is <span class="tag is-success">{{this.bill}}</span></h5>
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script>
+  export default {
+    name: 'amount',
+    data() {
+      return {
+        amount: null,
+        gst: null,
+        bill: null
+      }
+    },
+    methods: {
+      calculateTotal() {
+        let gst = parseFloat(this.amount) * 0.07
+
+        this.gst = gst.toFixed(2)
+
+        this.bill = parseFloat(this.amount) + parseFloat(this.gst)
+    }
+  }
+}
+</script>
---
+++ 12/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <div id="app">
+      <div class="columns is-centered mt-10">
+        <div class="column is-half">
+          <img src="http://localhost/~donda/playground/vue/12/dist/logo.png">
+        </div>
+        <div class="column is-half">
+          <h1>{{ msg }}</h1>
+        </div>
+      </div>
+      <amount></amount>
+      
+    </div>
+  </div>
+
+</template>
+
+<script>
+  import Amount from './Amount.vue'
+
+  export default {
+    name: 'app',
+    components: { Amount },
+    data () {
+      return {
+        msg: 'Welcome to My GST Calculator'
+      }
+    }
+  }
+</script>
+
+<style type="text/css">
+
+body {
+  padding-top: 40px;
+}
+</style>
